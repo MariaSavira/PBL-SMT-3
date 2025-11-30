d0daaf854cd189271a8076b464f496956de97d28
MariaSavira <mariasavira2006@gmail.com>
2025-11-30 10:28:24 +0700

    Sidebar done, Navbar kurang dropdown

--- Assets/Javascript/HeaderFooter.js
+++ Assets/Javascript/HeaderFooter.js
@@ -1,25 +1,19 @@
 const currentPage = window.location.pathname;
 
-function loadCSS(url){
+function loadCSS(url) {
     const link = document.createElement("link");
     link.rel = "stylesheet";
     link.href = url;
     document.head.appendChild(link);
 }
 
-document.querySelectorAll(".menu span").forEach(a => {
-    if (a.getAttribute("href") === currentPage.split("/").pop()) {
-        a.classList.add("active");
-
-        console.log(a.getAttribute())
-    }
-});
-
-async function loadComponent(id, file, url){
+async function loadComponent(id, file, url) {
     loadCSS(url);
     const target = document.getElementById(id);
     const result = await fetch(file); //ambil hasil dari server
     const html = await result.text(); //konversi data mentah menjadi html
+    // console.log(target);
+    // console.log(html);
     target.innerHTML = html;
     return target;
 }
@@ -29,22 +23,39 @@ document.addEventListener("DOMContentLoaded", async function () {
     // loadCSS("../Assets/Css/HeaderFooter.css");
     await loadComponent("footer", "Footer.html", "../Assets/Css/HeaderFooter.css");
 
+    console.log(currentPage.split("/").pop())
+
     const navbar = document.querySelector(".navbar");
     // const menu = document.querySelector(".menu");
     const hero = document.querySelector("#hero");
 
     window.addEventListener("scroll", function () {
-        const scrollTrigger = hero.offsetHeight;
-        
-        if (window.scrollY >= scrollTrigger) {
-            // menu.classList.add("menu-scrolled");
-            navbar.classList.add("navbar-scrolled");
-            navbar.style.position = "fixed";
-        } else {
-            // menu.classList.remove("menu-scrolled");
-            navbar.classList.remove("navbar-scrolled");
-            navbar.style.position = "absolute";
+        if (currentPage === "/User/Index.html") {
+            const scrollTrigger = hero.offsetHeight;
+
+            if (window.scrollY >= scrollTrigger) {
+                // menu.classList.add("menu-scrolled");
+                navbar.classList.add("navbar-scrolled");
+                navbar.style.position = "fixed";
+            } else {
+                // menu.classList.remove("menu-scrolled");
+                navbar.classList.remove("navbar-scrolled");
+                navbar.style.position = "absolute";
+            }
         }
+    });
+
+    document.querySelectorAll(".menu span a").forEach(a => {
+        // if (a.getAttribute("href") === currentPage.split("/")) {
+        //     a.classList.add("active");
+        //     console.log(a.getAttribute("href"))
+        // }
+        // const link = a.getAttribute("href");
+
+        if (a.getAttribute("href") === currentPage.split("/").pop()){
+            a.classList.add("active");
+        }
+        // console.log(link);
     });
 });
 
@@ -53,6 +64,6 @@ const menuLogin = document.getElementById('login');
 document.addEventListener("keydown", (e) => {
     if (e.ctrlKey && e.key === "l") {
         e.preventDefault();
-            menuLogin.style.display = block;
+        menuLogin.style.display = block;
     }
 });
